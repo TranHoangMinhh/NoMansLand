60e3df6e3b98eb3dcd4530c2f8861c3867df684e
19127406 <63275057+19127406@users.noreply.github.com>
2023-05-17 09:53:33 +0700

    update spawn weapons

---
+++ NoMansLand_Client/Assets/Scripts/Map/WeaponsSpawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WeaponsSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject weaponsSpawner;
+    [SerializeField] private List<Loot> weapons;
+
+    private Loot GetDroppedWeapon()
+    {
+        int randomChance = Random.Range(1, 101);
+        List<Loot> loots = new List<Loot>();
+
+        foreach (Loot loot in weapons)
+        {
+            if(randomChance <= loot.GetDroppedChance())
+            {
+                loots.Add(loot);
+            }
+        }
+
+        if(loots.Count > 0)
+        {
+            Loot droppedWeapon = loots[Random.Range(0, loots.Count)];
+            return droppedWeapon;
+        }
+
+        Debug.Log("No Weapon Is Spawned!!!");
+        return null;
+    }
+
+    public void InstantiateLoot(Vector3 spawnPosition)
+    {
+        Loot weapon = GetDroppedWeapon();
+        if( weapon != null )
+        {
+            Instantiate(weapon.GetWeapon(), spawnPosition, Quaternion.identity);
+        }
+    }
+
+    
+}
