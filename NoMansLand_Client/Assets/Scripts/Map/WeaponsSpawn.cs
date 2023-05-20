429114ea7c250d47cb669bd274ea5205b4df7355
19127406 <63275057+19127406@users.noreply.github.com>
2023-05-20 21:49:59 +0700

    add gitignore to test

--- NoMansLand_Client/Assets/Scripts/Map/WeaponsSpawn.cs
+++ NoMansLand_Client/Assets/Scripts/Map/WeaponsSpawn.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class WeaponsSpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject weaponsSpawner;
+    //[SerializeField] private GameObject weaponsSpawner;
     [SerializeField] private List<Loot> weapons;
 
     private Loot GetDroppedWeapon()
     {
         int randomChance = Random.Range(1, 101);
+
         List<Loot> loots = new List<Loot>();
 
         foreach (Loot loot in weapons)
---
+++ NoMansLand_Client/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            IInteractable interactable = GetInteractableObject();
+            if (interactable != null)
+            {
+                interactable.Interact();
+            }
+        }
+    }
+
+    public IInteractable GetInteractableObject()
+    {
+        float interactRange = 2f;
+        List<IInteractable> interactableList = new List<IInteractable>();
+        Collider[] colliders = Physics.OverlapSphere(transform.position, interactRange);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactableList.Add(interactable);
+            }
+        }
+
+        IInteractable closestInteractable = null;
+        foreach (IInteractable interactable in interactableList)
+        {
+            if (closestInteractable == null)
+            {
+                closestInteractable= interactable;
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, interactable.GetTransform().position) < Vector3.Distance(transform.position, closestInteractable.GetTransform().position))
+                {
+                    closestInteractable = interactable;
+                }
+            }
+        }
+
+        return closestInteractable;
+    }
+}
