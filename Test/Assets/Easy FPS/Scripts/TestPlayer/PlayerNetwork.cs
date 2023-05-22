7ba45e48edfaff678b3cdf367a09dece1258ac1f
Tran <minhtrnhong662@gmail.com>
2023-05-22 12:35:49 +0700

    change prefab

--- Test/Assets/Easy FPS/Scripts/TestPlayer/PlayerNetwork.cs
+++ Test/Assets/Easy FPS/Scripts/TestPlayer/PlayerNetwork.cs
@@ -6,6 +6,7 @@ using Unity.Netcode;
 public class PlayerNetwork : NetworkBehaviour
 {
     private void Update(){
+        if(!IsOwner) return;
         Vector3 moveDir = new Vector3(0,0,0);
 
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
@@ -13,6 +14,8 @@ public class PlayerNetwork : NetworkBehaviour
         if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
         if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
 
+        float moveSpeed = 5f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 
 }
