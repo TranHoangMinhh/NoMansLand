1cfe2259229d5d58b073c387b835fc3eb86d76ad
khoivankarik <khoivankarik@gmail.com>
2023-07-06 17:01:27 +0700

    grenade and smoke

--- Test/Assets/ThrowGrenade.cs
+++ Test/Assets/ThrowGrenade.cs
@@ -4,7 +4,8 @@ using Unity.Collections;
 
 public class ThrowGrenade : MonoBehaviour
 {
-    public GameObject grenadePrefab;
+    public GameObject smokeGrenadePrefab;
+    public GameObject explosionGrenadePrefab;
     public float throwForce = 10f;
     public int grenadeDamage = 50;
     public float explosionRadius = 10f;
@@ -20,20 +21,38 @@ public class ThrowGrenade : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
+            ThrowSmokeGrenade();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
             ThrowExplosionGrenade();
         }
     }
 
-    private void ThrowExplosionGrenade()
+    private void ThrowSmokeGrenade()
     {
         // Instantiate the smoke grenade prefab
-        GameObject smokeGrenade = Instantiate(grenadePrefab, transform.position, Quaternion.identity);
+        GameObject smokeGrenade = Instantiate(smokeGrenadePrefab, transform.position, Quaternion.identity);
 
         // Get the forward direction of the player's camera
         Vector3 throwDirection = mainCamera.transform.forward;
 
         // Apply the throw force to the smoke grenade in the throw direction
         Rigidbody rb = smokeGrenade.GetComponent<Rigidbody>();
+        rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
+    }
+
+    private void ThrowExplosionGrenade()
+    {
+        // Instantiate the explosion grenade prefab
+        GameObject explosionGrenade = Instantiate(explosionGrenadePrefab, transform.position, Quaternion.identity);
+
+        // Get the forward direction of the player's camera
+        Vector3 throwDirection = mainCamera.transform.forward;
+
+        // Apply the throw force to the explosion grenade in the throw direction
+        Rigidbody rb = explosionGrenade.GetComponent<Rigidbody>();
         rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
     }
 }
